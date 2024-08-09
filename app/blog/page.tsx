b0ae75c05439eeae4eb0ccd7cbe61c2c2b6c67c4
fuzzbear <58638135+fuzzbear0001@users.noreply.github.com>
2024-08-09 17:12:54 +0100

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,6 @@
 import { title } from "@/components/primitives";
 
-export default function DocsPage() {
+export default function BlogPage() {
   return (
     <div>
       <h1 className={title()}>Nothing here yet..</h1>
