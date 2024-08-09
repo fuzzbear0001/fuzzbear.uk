131c4fc29fbfa47ef79eb0955ac1e0b0067f2fd4
fuzzbear <58638135+fuzzbear0001@users.noreply.github.com>
2024-08-09 18:00:25 +0100

    Update page.tsx

--- app/hof/page.tsx
+++ app/hof/page.tsx
@@ -7,12 +7,56 @@ import cards from "@/json/hof.json";
 
 
 export default function Hof() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // Function to filter cards based on the search term
+  const filteredCards = cardsData.filter((card) =>
+    card.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
-      <h1 className="rainbow-wave text-3xl lg:text-4xl">Hall Of Autism</h1>
+      <h1 className="rainbow-wave text-3xl lg:text-4xl text-center">Hall Of Autism</h1>
+      
+      {/* Centered Search Input */}
+      <div className="flex justify-center mt-4">
+        <Input
+          label="Search"
+          isClearable
+          radius="lg"
+          classNames={{
+            label: "text-black/50 dark:text-white/90",
+            input: [
+              "bg-transparent",
+              "text-black/90 dark:text-white/90",
+              "placeholder:text-default-700/50 dark:placeholder:text-white/60",
+            ],
+            innerWrapper: "bg-transparent",
+            inputWrapper: [
+              "shadow-xl",
+              "bg-default-200/50",
+              "dark:bg-default/60",
+              "backdrop-blur-xl",
+              "backdrop-saturate-200",
+              "hover:bg-default-200/70",
+              "dark:hover:bg-default/70",
+              "group-data-[focus=true]:bg-default-200/50",
+              "dark:group-data-[focus=true]:bg-default/60",
+              "!cursor-text",
+            ],
+          }}
+          placeholder="Type to search..."
+          startContent={
+            <SearchIcon className="text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
+          }
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+
       <div className="mt-4">
         <div className="gap-2 grid grid-cols-2 sm:grid-cols-2">
-          {cards.map((card, index) => (
+          {filteredCards.map((card, index) => (
             <Card className="py-4" key={index}>
               <CardHeader className="pb-0 pt-2 px-4 flex items-center justify-center">
                 <div className="flex items-center justify-center">
