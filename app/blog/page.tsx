eba26e1db9e45097b040494d64ac44426f4510c9
fuzzbear <58638135+fuzzbear0001@users.noreply.github.com>
2024-08-09 17:07:33 +0100

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,1 +1,9 @@
+import { title } from "@/components/primitives";
 
+export default function DocsPage() {
+  return (
+    <div>
+      <h1 className={title()}>Nothing here yet..</h1>
+    </div>
+  );
+}
