522d18b91edb7d2572f0065488be825c7d7ad659
fuzzbear <58638135+fuzzbear0001@users.noreply.github.com>
2024-08-09 17:02:15 +0100

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,1 @@
-"use client"
-import {useEffect} from 'React'
-export default function redirect() {
-    useEffect(() => {
-        window.location.assign('https://www.z2u.com/items-9912888/snapchat-accounts-1million-score-original-email-auto-delivery.html?seller=249432')
-    })
-    return(
-        <>
-        </>
-    )
-}
+
