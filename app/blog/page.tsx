fd223ee30bf823ae9ba72fb4ddda6ab541cc322d
fuzzbear <58638135+fuzzbear0001@users.noreply.github.com>
2024-08-09 17:19:07 +0100

    Delete app/blog directory

--- app/blog/page.tsx
+++
@@ -1,9 +0,0 @@
-import { title } from "@/components/primitives";
-
-export default function BlogPage() {
-  return (
-    <div>
-      <h1 className={title()}>Nothing here yet..</h1>
-    </div>
-  );
-}
